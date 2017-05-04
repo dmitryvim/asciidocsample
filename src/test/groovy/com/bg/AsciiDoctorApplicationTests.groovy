8f80494de67d920b686b7ac4c7353731ece76d64
Dmitry Mikhaylovich <dmitry.mikhailovich@gmail.com>
2017-05-04 09:09:43 +0300

    start rest docs

--- src/test/groovy/com/bg/AsciiDoctorApplicationTests.groovy
+++ src/test/groovy/com/bg/AsciiDoctorApplicationTests.groovy
@@ -1,16 +1,19 @@
 package com.bg
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 /**
  * @author dmitry.mikhaylovich@bostongene.com
  */
+@AutoConfigureRestDocs(outputDir = "build/generated-snippets")
 @WebMvcTest(Controller)
 class AsciiDoctorApplicationTests extends Specification {
 
@@ -25,6 +28,7 @@ class AsciiDoctorApplicationTests extends Specification {
         expect:
         this.mockMvc.perform(get("/get/$value"))
                 .andExpect(status().isOk())
+                .andDo(document("index"));
 
     }
 
